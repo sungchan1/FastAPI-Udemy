babc6db048773119c562a57096b12b008355a9ca
sungchan <sungchan254@naver.com>
2024-08-11 22:22:12 +0900

    Finish Project 2 - 1

---
+++ books2.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int | None = Field(description="ID is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "author": "author",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithruby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithruby', 'A great book', 5),
+    Book(3, 'Master Endpoints', 'codingwithruby', 'A very nice book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book description', 3),
+    Book(5, 'HP2', 'Author 2', 'Book description', 2),
+    Book(6, 'HP3', 'Author 3', 'Book description', 1),
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else len(BOOKS) + 1
+    return book
