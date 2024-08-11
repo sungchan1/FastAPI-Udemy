8c1c3ce546d6d119677af33a5f8b2645c0403ab2
sungchan <sungchan254@naver.com>
2024-08-11 18:43:44 +0900

    Finish Project 1

--- books.py
+++ books.py
@@ -30,6 +30,14 @@ async def read_all_books():
 
 
 
+@app.get("/books/byauthor/")
+async def read_books_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
 
 
 
@@ -74,5 +82,21 @@ async def read_books_by_author(category: str, book_author: str):
                 book.get('category').casefold() == category.casefold():
             books.append(book)
     return books
+
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
 
 
